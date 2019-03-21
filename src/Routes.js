b8b8b9be063473b8471bd3f48b1ff82c0daaae8b
yannyyss <GitYan12>
2019-03-21 10:54:59 -0600

    routes modified

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Container/Home/Home";
 import CampaignCalendar from "./Container/CampaignCalendar/CampaignCalendar";
+import ProfitSimulator from "./Container/ProfitSimulator/ProfitSimulator";
 
 export default () => (
   <Switch>
     <Route path="/home" component={Home} />
     <Route path="/calendar" component={CampaignCalendar} />
+    <Route path="/profitsimulator" component={ProfitSimulator} />
   </Switch>
 );
